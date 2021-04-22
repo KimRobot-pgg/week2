6ee82c38e65a5f60e2ed67864e5c8ba6ecd65b3f
KimRobot <pbung1st@gmail.com>
2021-04-22 11:05:53 +0900

    first commit

---
+++ src/main/java/Code.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("알고리즘 문제 선택");
+        System.out.println("1. 1~n 연속된 수의 합");
+        System.out.println("2. 두 수 입력, n~m 의 합");
+        System.out.println("3. 별찍기 숫자입력,마름모 출력");
+        System.out.println("4. 별찍기응용1 육망성");
+        System.out.println("5. 별찍기응용2 오망성");
+        System.out.println("6. 별찍기응용3 원");
+        System.out.println("7. 스도쿠");
+        System.out.println("8. 초급알고리즘. 가장 긴 문자열");
+
+
+
+    }
+}
